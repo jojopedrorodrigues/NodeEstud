8e8462571897e74bd74277e61d6d950b340de755
jojopedrorodrigues <jlabssistem@gmail.com>
2023-05-25 21:09:22 -0300

    inputAndEventEmmiter

--- index.mjs
+++ index.mjs
@@ -1,16 +1,9 @@
-import chalk from 'chalk';
-import diminuir from './modulo.mjs';
-import inquirer from 'inquirer'
+import inquirer from "inquirer";
+import EventEmitter from "events";
+const eventEmitter = new EventEmitter();
 
-inquirer.prompt(
-    [
-        { name: 'p1', message: 'Qual é a nota 01? ' },
-        { name: 'p2', message: 'Qual é a nota 02? ' }
-    ]
-).then(
-    (resposta) => console.log(resposta)
-).catch((err) =>
-    console.log(err)
-);
-
-
+eventEmitter.on('start', () => {
+    inquirer.prompt([{ name: "p1", message: "Number" }]).then((res) => console.log(res.p1)).catch((err) => console.log(err));
+    console.log('start programa');
+});
+eventEmitter.emit('start');
