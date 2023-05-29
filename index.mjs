4d6ebcc0323dfe8e93ab1dfc92a43c819b86ff47
jojopedrorodrigues <jlabssistem@gmail.com>
2023-05-28 21:39:47 -0300

    servidorPort3000AndGravaçãoArq

--- index.mjs
+++ index.mjs
@@ -1,17 +1,15 @@
 import EventEmitter from "events";
-import fs from "fs";
-import http from 'http';
+import connectServer from "./modulo.mjs";
+
 const port = 3000;
-const server = http.createServer((requisition, response) => {
-    response.write('Oi Http server');
-    response.end();
-});
-server.listen(port, function () { console.log('servidor rodando ' + port) });
+
+
+
 const eventEmitter = new EventEmitter();
-const fileSystem = fs;
+
 
 eventEmitter.on('start', () => {
-
+    connectServer(port);
 });
 
 eventEmitter.emit('start');
--- modulo.mjs
+++ modulo.mjs
@@ -1,6 +1,36 @@
-function diminuir(a,b){
-    console.log(a-b);
-    return a-b;
+import fs from "fs";
+import http from 'http';
+
+const port = 3000;
+
+async function writeResponseServer(port) {
+    try {
+        let x = 10;
+        x = '${port}';
+        fs.writeFile('arqddd.txt', 'Porta rodando = ' + String(port), (error) => {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+function connectServer(port) {
+    const server = http.createServer((requisition, response) => {
+        response.write('Oi Http server');
+        response.end();
+    });
+
+    server.listen(port,
+        function () {
+            console.log('servidor rodando ' + port),
+                writeResponseServer(port);
+        }
+    );
+
 }
 
-export default diminuir;
+
+export default connectServer;
